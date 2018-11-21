5d178c8184dafc56c4be4b56a7dbe94c525da137
Jiayi Liu <liujiayi19920724@gmail.com>
2018-11-21 17:00:47 +1300

    initial commit, add pods, resolve dependencies, create cells

---
+++ DietCalender/View/Cells/JLCalenderCell.swift
@@ -0,0 +1,19 @@
+//
+//  JLCalenderCell.swift
+//  DietCalender
+//
+//  Created by Beethoven on 21/11/18.
+//  Copyright © 2018 Jiayi Liu. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class JLCalenderCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ DietCalender/View/Cells/JLEventCell.swift
@@ -0,0 +1,24 @@
+//
+//  JLEventCell.swift
+//  DietCalender
+//
+//  Created by Beethoven on 21/11/18.
+//  Copyright © 2018 Jiayi Liu. All rights reserved.
+//
+
+import UIKit
+
+class JLEventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
