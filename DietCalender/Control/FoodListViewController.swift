95d6ddddcae5a610e46f24806051aea80ec88dd0
Jiayi Liu <liujiayi19920724@gmail.com>
2018-11-28 17:54:49 +1300

    add food list view controller

---
+++ DietCalender/Control/FoodListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FoodListViewController.swift
+//  DietCalender
+//
+//  Created by Beethoven on 28/11/18.
+//  Copyright © 2018 Jiayi Liu. All rights reserved.
+//
+
+import UIKit
+
+class FoodListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
